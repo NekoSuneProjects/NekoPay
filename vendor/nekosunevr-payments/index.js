de63f666a2687e150205c51a963e5374fda42317
NekoSuneVR <Chisdeal2014@hotmail.com>
2026-08-26 16:35:05 +0100

    chore: route legacy imports to NekoPay SDK

---
+++ vendor/nekosunevr-payments/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+// Transitional package name only. There is no external NekoSuneVR/nekosunevr-payments
+// dependency anymore: legacy require('nekosunevr-payments') calls resolve to NekoPay.
+module.exports = require('../../index');
